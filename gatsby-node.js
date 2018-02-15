3e588cf40e02d943e7aaeba90811660a6fec42b9
Hemant Joshi <codingdefined@gmail.com>
2018-02-15 11:33:21 +0530

    Updated to .then()

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,25 +3,25 @@ const utopian = require('utopian-api');
 
 exports.sourceNodes = async ({ boundActionCreators }, { user }) => {
   const { createNode } = boundActionCreators;
-  const utopianPosts = await utopian.getPostByAuthor(user);
+  utopian.getPostByAuthor('codingdefined').then((data) => {
+    data.results.forEach(post => {
+      const nodeStr = JSON.stringify(post);
+      const nodeHash = crypto
+        .createHash('md5')
+        .update(nodeStr)
+        .digest('hex');
 
-  utopianPosts.results.forEach(post => {
-    const nodeStr = JSON.stringify(post);
-    const nodeHash = crypto
-      .createHash('md5')
-      .update(nodeStr)
-      .digest('hex');
-
-    createNode({
-      ...post,
-      id: `${post.id}`,
-      parent: "root",
-      children: [],
-      internal: {
-        type: 'UtopianPost',
-        content: nodeStr,
-        contentDigest: nodeHash,
-      },
+      createNode({
+        ...post,
+        id: `${post.id}`,
+        parent: "root",
+        children: [],
+        internal: {
+          type: 'UtopianPost',
+          content: nodeStr,
+          contentDigest: nodeHash,
+        },
+      });
     });
   });
   return;
